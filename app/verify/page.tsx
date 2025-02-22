dcb99aa9ebe7dcc744d057a6fefc2361363f6751
Chirag <22btrsn010+github@jainuniversity.ac.in>
2025-02-22 18:49:20 +0530

    domain added

--- app/verify/page.tsx
+++ app/verify/page.tsx
@@ -15,7 +15,7 @@ export default function Verify(){
                         Check your Email
                     </CardTitle>
                     <CardDescription>
-                        We&aposve sent a verification link to your email address.
+                        We have sent a verification link to your email address.
                     </CardDescription>
                 </CardHeader>
                 <CardContent>

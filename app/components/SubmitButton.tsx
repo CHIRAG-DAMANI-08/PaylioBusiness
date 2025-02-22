9dbc9cad95f03b05703e9928bb9f460d79b7574f
Chirag <22btrsn010+github@jainuniversity.ac.in>
2025-02-22 15:49:26 +0530

    first commit

---
+++ app/components/SubmitButton.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { useFormStatus } from "react-dom";
+import { Loader2 } from "lucide-react"
+
+interface iAppProps{
+    text:string;
+    
+}
+
+export function SubmitButton({text}:iAppProps){
+    const {pending} = useFormStatus();
+    return <>{pending ?( <Button disabled className="w-full"><Loader2 className="size-4 mr-3 animate-spin"/>Please Wait...</Button>):(<Button type="submit" className="w-full">{text}</Button>)}</>
+}
---
+++ app/verify/page.tsx
@@ -0,0 +1,41 @@
+import { buttonVariants } from "@/components/ui/button";
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import { AlertCircle, ArrowLeft, Mail } from "lucide-react";
+import Link from "next/link";
+
+export default function Verify(){
+     return(
+        <div className="min-h-screen w-full flex items-center justify-center">
+            <Card className="w-[380px] px-5">
+                <CardHeader className="text-center ">
+                    <div className="mb-4 mx-auto flex size-20 items-center justify-center rounded-full bg-blue-200">
+                        <Mail className="size-12 text-blue-500"/>
+                    </div>
+                    <CardTitle className="text-2xl font-bold">
+                        Check your Email
+                    </CardTitle>
+                    <CardDescription>
+                        We've sent a verification link to your email address.
+                    </CardDescription>
+                </CardHeader>
+                <CardContent>
+                    <div className="mt-4 rounded-medium bg-yellow-50 border-yellow-300 p-4">
+                        <div className="flex items-center">
+                            <AlertCircle className="size-5 text-yellow-500"/>
+
+                            <p className="text-sm font-medium text-yellow-700 ml-3">Check your spam folder too!</p>
+                        </div>
+                    </div>
+                </CardContent>
+                <CardFooter>
+                    <Link href="/" className={buttonVariants({
+                        className: "w-full",
+                        variant: "outline"
+                    })}>
+                        <ArrowLeft className="mr-2 size-4"/>Back to Homepage.
+                    </Link>
+                </CardFooter>
+            </Card>
+        </div>
+     )
+}
